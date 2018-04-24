5e8c7a3543a9e2725b09713c809bc772561bf458
Naresh Kavali <naresh.kavali@gmail.com>
2018-04-25 00:42:19 +0530

    More cahnges

--- public/modules/dashboard/dashboard.controller.js
+++ public/modules/dashboard/dashboard.controller.js
@@ -15,71 +15,55 @@
         dc.support = ["Email", "Chat", "800-SiteSup"];
         dc.location_opted = {};
 
-        dc.carousel = $('.owl-carousel').owlCarousel({
-            items: 5,
-            dots: false,
-            autoplay: true,
-            loop: true,
-            margin: 10,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            autoplayHoverPause: true,
-            lazyLoad: true
-        });
 
-        var w = $('.history_image').width();
-        $('.history_image').height((w / 1.78));
 
-        dc.carousel.on('changed.owl.carousel', function(property) {
-            var current = property.item.index;
-            var item = $(property.target).find(".item").eq(current);
-            $(property.target).find(".item").css('border', '0px solid red');
-            $(item).css('border', '3px solid #4388f6');
+        // var w = $('.history_image').width();
+        // $('.history_image').height((w / 1.78));
 
-            var pos = $(item).find("img").attr('pos');
-            if (pos && dc.location_history && dc.location_history.length > 0) {
-                var rec = dc.location_history[pos];
-                $('.history_image').attr('src', rec.image);
-                $('.site-hist-name').html(rec.name);
-                $('.site-hist-date').html($filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm'));
-                $('.site-hist-temp').html(rec.temperature);
-                $('.site-hist-humid').html(rec.humidity);
-                console.log(JSON.stringify(dc.site_his_rec));
-            }
-            // console.log('Image position ' + pos);
-            //dc.site_his_rec = dc.location_history[pos];
-        });
+        // dc.carousel.on('changed.owl.carousel', function(property) {
+        //     var current = property.item.index;
+        //     var item = $(property.target).find(".item").eq(current);
+        //     $(property.target).find(".item").css('border', '0px solid red');
+        //     $(item).css('border', '3px solid #4388f6');
 
-        dc.carousel.on('click', '.item', function(property) {
-            var pos = $(property.target).attr('pos');
-            var item = $(property.target);
-            $('.owl-carousel').find(".item").css('border', '0px solid red');
-            $(item).parent().css('border', '3px solid #4388f6');
+        //     var pos = $(item).find("img").attr('pos');
+        //     if (pos && dc.location_history && dc.location_history.length > 0) {
+        //         var rec = dc.location_history[pos];
+        //         $('.history_image').attr('src', rec.image);
+        //         $('.site-hist-name').html(rec.name);
+        //         $('.site-hist-date').html($filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm'));
+        //         $('.site-hist-temp').html(rec.temperature);
+        //         $('.site-hist-humid').html(rec.humidity);
+        //         console.log(JSON.stringify(dc.site_his_rec));
+        //     }
+        //     // console.log('Image position ' + pos);
+        //     //dc.site_his_rec = dc.location_history[pos];
+        // });
 
-            if (pos && dc.location_history && dc.location_history.length > 0) {
-                var rec = dc.location_history[pos];
-                $('.history_image').attr('src', rec.image);
-                $('.site-hist-name').html(rec.name);
-                $('.site-hist-date').html($filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm'));
-                $('.site-hist-temp').html(rec.temperature);
-                $('.site-hist-humid').html(rec.humidity);
-                console.log(JSON.stringify(dc.site_his_rec));
-            }
-        });
+        // dc.carousel.on('click', '.item', function(property) {
+        //     var pos = $(property.target).attr('pos');
+        //     var item = $(property.target);
+        //     $('.owl-carousel').find(".item").css('border', '0px solid red');
+        //     $(item).parent().css('border', '3px solid #4388f6');
+
+        //     if (pos && dc.location_history && dc.location_history.length > 0) {
+        //         var rec = dc.location_history[pos];
+        //         $('.history_image').attr('src', rec.image);
+        //         $('.site-hist-name').html(rec.name);
+        //         $('.site-hist-date').html($filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm'));
+        //         $('.site-hist-temp').html(rec.temperature);
+        //         $('.site-hist-humid').html(rec.humidity);
+        //         console.log(JSON.stringify(dc.site_his_rec));
+        //     }
+        // });
 
 
         dc.showLocations = function() {
-            $('.owl-carousel').trigger('stop.owl.autoplay');
+            $('#owl-frame').html('');
+            $('#owl-thumb-frame').html('');
             dc.location_history = null;
         };
 
-        dc.showFullImage = function($event) {
-            var card = $($event.currentTarget).closest('.card');
-            var imgSrc = $(card).find('.history_image').attr('src');
-            $('.img-zoom').attr('src', imgSrc);
-            $('#myModalLabel').html($(card).find('.site-hist-name').html() + '  <small class="font-weight-300">' + $(card).find('.site-hist-date').html() + '</small>');
-            $('#myModal').modal('show');
-        };
 
         dc.toggleContainer = function($event, container) {
             $event.stopPropagation();
@@ -101,24 +85,32 @@
             $('.owl-carousel').trigger('stop.owl.autoplay');
             dc.location_history = [];
 
+            $('#owl-frame').html('');
+            $('#owl-thumb-frame').html('');
+
             var loadingEl = $('#locations-history');
             blockUI(loadingEl);
             UserService.GetLocationImagesHistory(data, function(err, imageRecs) {
                 unblockUI(loadingEl);
                 if (!err) {
                     dc.location_history = imageRecs;
-
-                    var items = $('.owl-carousel').find('.item');
-                    for (var i = 0; i < items.length; i++) {
-                        $(".owl-carousel").trigger('remove.owl.carousel', [i])
-                            .trigger('refresh.owl.carousel');
-                    }
+                    var location_id = dc.location_opted.location_id + (new Date().getMilliseconds());
+                    createOwlCarousels(location_id);
 
                     for (var i in dc.location_history) {
-                        $('.owl-carousel').trigger('add.owl.carousel', ['<div class="item"><img pos="' + i + '" src="' + dc.location_history[i].image_thumbnail + '" alt=""></div>']);
+                        var rec = dc.location_history[i];
+                        rec.date = $filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm');
+                        var imgCard = '<div class="item" pos="' + i + '"><div class="card"><img class="card-img-top" src="' + rec.image + '"><div class="card-img-overlay"><div class="card-title"><h6 class="text-success site">' + rec.name + '</h6><button class="btn btn-sm btn-success ml-3 pull-right full-image" data-toggle="tooltip" title="Full Image"><i class="fa fa-external-link"></i></button></div><div class="text-primary font-weight-300 date">' + rec.date + '</div><p class="card-text text-primary font-weight-300 py-3"><span><i class="fa fa-thermometer"></i> Temperature <span>' + rec.temperature + '</span></span><span class="ml-3"><i class="fa fa-tint"></i> Humidity <span>' + rec.humidity + '</span></span></p></div></div></div>';
+
+                        $('#owl-thumb-' + location_id).trigger('add.owl.carousel', ['<div class="item" pos="' + i + '"><img src="' + dc.location_history[i].image_thumbnail + '" alt=""></div>']);
+                        $('#owl-' + location_id).trigger('add.owl.carousel', [imgCard]);
                     }
-                    $('.owl-carousel').trigger('refresh.owl.carousel');
-                    $('.owl-carousel').trigger('play.owl.autoplay', [3000]);
+
+                    $('#owl-thumb-' + location_id).trigger('refresh.owl.carousel');
+                    $('#owl-' + location_id).trigger('refresh.owl.carousel');
+                    $('#owl-' + location_id).trigger('stop.owl.carousel');
+
+                    $('#owl-thumb-' + location_id).trigger('play.owl.autoplay', [3000]);
 
                     //console.log(JSON.stringify(dc.location_history));
                 } else {
@@ -138,8 +130,10 @@
             data.from = $filter('date')(date2, "yyyy-MM-dd");
 
             dc.location_opted = location;
-            $('.owl-carousel').trigger('stop.owl.autoplay');
             dc.location_history = [];
+
+            $('#owl-frame').html('');
+            $('#owl-thumb-frame').html('');
 
             var loadingEl = $('#locations-history');
             blockUI(loadingEl);
@@ -147,18 +141,30 @@
                 unblockUI(loadingEl);
                 if (!err) {
                     dc.location_history = imageRecs;
-
-                    var items = $('.owl-carousel').find('.item');
-                    for (var i = 0; i < items.length; i++) {
-                        $(".owl-carousel").trigger('remove.owl.carousel', [i])
-                            .trigger('refresh.owl.carousel');
-                    }
+                    var location_id = dc.location_opted.location_id + (new Date().getMilliseconds());;
+                    createOwlCarousels(location_id);
 
                     for (var i in dc.location_history) {
-                        $('.owl-carousel').trigger('add.owl.carousel', ['<div class="item"><img pos="' + i + '" src="' + dc.location_history[i].image_thumbnail + '" alt=""></div>']);
+                        var rec = dc.location_history[i];
+                        rec.date = $filter('date')(new Date(rec.timestamp), 'MMM dd, yyyy  HH:mm');
+                        var imgCard = '<div class="item" pos="' + i + '"><div class="card"><img class="card-img-top" src="' + rec.image + '"><div class="card-img-overlay"><div class="card-title"><h6 class="text-success site">' + rec.name + '</h6><button class="btn btn-sm btn-success ml-3 pull-right full-image" data-toggle="tooltip" title="Full Image"><i class="fa fa-external-link"></i></button></div><div class="text-primary font-weight-300 date">' + rec.date + '</div><p class="card-text text-primary font-weight-300 py-3"><span><i class="fa fa-thermometer"></i> Temperature <span>' + rec.temperature + '</span></span><span class="ml-3"><i class="fa fa-tint"></i> Humidity <span>' + rec.humidity + '</span></span></p></div></div></div>';
+
+                        $('#owl-thumb-' + location_id).trigger('add.owl.carousel', ['<div class="item" pos="' + i + '"><img src="' + dc.location_history[i].image_thumbnail + '" alt=""></div>']);
+                        $('#owl-' + location_id).trigger('add.owl.carousel', [imgCard]);
                     }
-                    $('.owl-carousel').trigger('refresh.owl.carousel');
-                    $('.owl-carousel').trigger('play.owl.autoplay', [3000]);
+
+                    $('#owl-thumb-' + location_id).trigger('refresh.owl.carousel');
+                    $('#owl-' + location_id).trigger('refresh.owl.carousel');
+                    $('#owl-' + location_id).trigger('stop.owl.carousel');
+
+                    $('#owl-thumb-' + location_id).trigger('play.owl.autoplay', [3000]);
+
+                    $('.full-image').on('click', function() {
+                        var imgSrc = $(this).closest('.card').find('img').attr('src');
+                        $('.img-zoom').attr('src', imgSrc);
+                        $('#myModalLabel').html($(card).find('.site-hist-name').html() + '  <small class="font-weight-300">' + $(card).find('.site-hist-date').html() + '</small>');
+                        $('#myModal').modal('show');
+                    });
 
                     //console.log(JSON.stringify(dc.location_history));
                 } else {
@@ -279,6 +285,74 @@
                     alert('An error occured !');
                     break;
             }
+        }
+
+
+
+
+        function createOwlCarousels(id) {
+            $('#owl-thumb-frame').html('<div id="owl-thumb-' + id + '" class="owl-carousel owl-carousel owl-theme owl-loaded owl-drag"></div>');
+            $('#owl-frame').html('<div id="owl-' + id + '" class="owl-carousel owl-carousel owl-theme owl-loaded owl-drag"></div>');
+
+            var owlThumb = $('#owl-thumb-' + id).owlCarousel({
+                items: 5,
+                dots: false,
+                autoplay: true,
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                lazyLoad: true
+            });
+
+            var owl = $('#owl-' + id).owlCarousel({
+                items: 1,
+                autoplay: false,
+                autoPlay: false,
+                dots: false
+            });
+
+            owlThumb.on('changed.owl.carousel', function(property) {
+                var current = property.item.index;
+                if (current) {
+                    var item = $(property.target).find(".item").eq(current);
+                    var pos = $(item).attr('pos');
+                    $(property.target).find(".item").css('border', '0px solid #4388f6');
+                    $(item).css('border', '3px solid #4388f6');
+
+                    if (pos) {
+                        owl.trigger('to.owl.carousel', pos);
+                    }
+                }
+            });
+
+            owlThumb.on('click', '.item', function(property) {
+                var pos = $(property.target).parent().attr('pos');
+                if (pos) {
+                    owlThumb.trigger('to.owl.carousel', pos);
+                }
+            });
+
+            owl.on('click', '.item', function(property) {
+                var el = $(property.target);
+                var n = el.get(0).nodeName;
+                if (n == 'I' || n == 'BUTTON') {
+                    var card = $(property.target).closest('.card');
+                    var imgSrc = card.find('img').attr('src');
+                    $('.img-zoom').attr('src', imgSrc);
+                    $('#myModalLabel').html($(card).find('.site').html() + '  <small class="font-weight-300">' + $(card).find('.date').html() + '</small>');
+                    $('#myModal').modal('show');
+                }
+            });
+
+            owl.on('mouseover', function(property) {
+                owlThumb.trigger('mouseover');
+            });
+
+            owl.on('mouseleave', function(property) {
+                owlThumb.trigger('mouseleave');
+            });
         }
 
         getProjects();
