6af8735ac763cc27fefa2936b29c92f3d55bce34
Naresh Kavali <naresh.kavali@gmail.com>
2018-04-27 10:40:31 +0530

    More cahanges

--- routes/cron.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var dbPool = require('./dbpool');
-var db = dbPool.getPool();
-var mysql = require('mysql');
-var moment = require('moment-timezone');
-
-
-module.exports = router;
